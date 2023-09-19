c354f1a654177f1bbdb1ea8c0990a9063cba955c
hknight <thale842@outlook.com>
2023-09-19 13:22:17 +0700

    update struct

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-import socketClient  from "socket.io-client";
-import {useEffect} from "react";
+// import configWebsite from './config_website.json'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-import configWebsite from './config_website.json'
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Oke</h1>,
+  },
+]);
 
 function App() {
-  useEffect(() => {
-    console.log(configWebsite)
-    let socket = socketClient(configWebsite.url)
-    socket.on('connection', () => {
-      console.log(`I'm connected with the back-end`);
-    });
-    fetch(configWebsite.url+'/api/bird').then(r => console.log(r))
-  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <RouterProvider router={router} />
+      </div>
   );
 }
 
