d2c4321539baa628a072e003bfa88abfaf43e0cc
hknight <thale842@outlook.com>
2023-10-07 02:27:39 +0700

    fix auth template

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -62,13 +62,13 @@ const Auth = () => {
                         </div>
                     </div>
                 </div>
-                <div className={"d-sm-none d-flex start-50 translate-middle-x scroll-button"}>
-                    <a href={`#Section2`}>
-                        <svg xmlns="http://www.w3.org/2000/svg" height="4em" viewBox="0 0 448 512">
-                            <path d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z" fill="#FFD0A1"/>
-                        </svg>
-                    </a>
-                </div>
+                <a href={`#Section2`}>
+                    <div className={"d-sm-none d-flex start-50 translate-middle-x scroll-button"}>
+                            <svg xmlns="http://www.w3.org/2000/svg" height="4em" viewBox="0 0 448 512">
+                                <path d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z" fill="#FFD0A1"/>
+                            </svg>
+                    </div>
+                </a>
             </div>
         </GoogleOAuthProvider>
     )
